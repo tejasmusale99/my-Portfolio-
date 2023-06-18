41e2a9308f01a08ea1c31597c1c7361ffa895a37
Tejas Musale <tejasmusale99@gmail.com>
2023-06-18 18:25:29 +0530

    Basic structure and basic css done

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <div className="app_sidebar">sidebar</div>
+          </div>
+          <div className="col-lg-9 app_main-content">main</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
