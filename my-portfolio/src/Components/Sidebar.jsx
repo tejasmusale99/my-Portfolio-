8a1602f99f103b43d0b5c296d8a333402f8970c3
Tejas Musale <tejasmusale99@gmail.com>
2023-07-06 11:47:18 +0530

    update resume

--- my-portfolio/src/Components/Sidebar.jsx
+++ my-portfolio/src/Components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Avatar from "../Icons/avatar.jpg";
-import resume from "../Resume/tejas_resume.pdf"
+import resume from "../Resume/resume_tejas.pdf"
 import { motion } from "framer-motion";
 
 function Sidebar() {
