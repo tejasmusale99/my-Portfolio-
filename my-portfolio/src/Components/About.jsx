602e7ea4214e111a86922659cf20016dd3c828d0
Tejas Musale <tejasmusale99@gmail.com>
2023-07-06 11:38:49 +0530

    change text in about section

--- my-portfolio/src/Components/About.jsx
+++ my-portfolio/src/Components/About.jsx
@@ -7,16 +7,14 @@ import { motion } from "framer-motion";
 function About() {
   const skills = [
     {
-      icon:webDesign,
+      icon: webDesign,
       title: "Web Design",
-      about:
-        "The most modern website design with interactive UI/UX.",
+      about: "The most modern website design with interactive UI/UX.",
     },
     {
-      icon:  webDevelop,
+      icon: webDevelop,
       title: "Web Development",
-      about:
-        "High-quality web apps at the professional level.",
+      about: "High-quality web apps at the professional level.",
     },
   ];
 
@@ -25,50 +23,67 @@ function About() {
       opacity: 0,
     },
     visible: {
-  
       opacity: 1,
-      transition:{
-        delay:0.1,duration:0.6,
-      }
+      transition: {
+        delay: 0.1,
+        duration: 0.6,
+      },
     },
-  }
+  };
 
-  return (  
+  return (
     <>
-    <motion.div className="about_info"
-      variants={about_variant}
-      initial="hidden"
-      animate="visible"
-    >
+      <motion.div
+        className="about_info"
+        variants={about_variant}
+        initial="hidden"
+        animate="visible"
+      >
+        <p className="about_para_first_line">
+          I'm <span className="text-highlight">Frontend Engineer</span> from
+          India, I build apps with a focus on{" "}
+          <span className="text-highlight">ReactJS.</span>.
+        </p>
 
+        <p className="about_para_lines">
+          I have experience building high-performing, responsive, beautiful web
+          applications to deliver a unique, intuitive user experience.I enjoy
+          being challenged and working on projects that require me to step
+          outside of my comfort zone.
+        </p>
 
-      <p className="about_para_first_line">
-        I'm <span className="text-highlight">Frontend Engineer</span> from India, I build apps with a focus on <span className="text-highlight">ReactJS.</span>.
-      </p>
+        <p className="about_para_lines">
+          Front End :
+          <span className="text-highlight">
+            HTML-5, CSS-3, JavaScript-ES6, React.js, React query, Redux.
+          </span>
+        </p>
 
-      <p className="about_para_lines">
-        I have experience building high-performing, responsive,
-        Attractive web applications to deliver a unique, intuitive user
-        experience. I enjoy being challenged and working on projects requiring me to step outside my comfort zone.
-      </p>
+        <p className="about_para_lines">
+          CSS Frameworks :
+          <span className="text-highlight"> Bootstrap-5, Tailwind CSS.</span>
+        </p>
 
-      <p className="about_para_lines">
-        Front End : <span className="text-highlight">HTML-5, CSS-3, JavaScript-ES6, React.js, React query, Redux.</span>
-      </p>
+        <p className="about_para_lines">
+          Tools I know :
+          <span className="text-highlight">
+            Postman, Git, Github,Chrome Dev tools.
+          </span>
+        </p>
 
-      <p className="about_para_lines">CSS Frameworks :<span className="text-highlight"> Bootstrap-5, Tailwind CSS.</span></p>
-
-      <p className="about_para_lines">Tools I know : <span className="text-highlight">Postman, Git, Github,Chrome Dev tools.</span></p>
-
-      <p className="about_para_lines">
-        Currently I am exploring : <span className="text-highlight">Node.js (Express.js), Firebase and MongoDB</span> in
-        my free time.
-      </p>
+        <p className="about_para_lines">
+          Currently I am exploring :
+          <span className="text-highlight">
+            Node.js (Express.js), Firebase and MongoDB
+          </span>{" "}
+          in my free time.
+        </p>
       </motion.div>
-      <motion.div className="row"
-       variants={about_variant}
-       initial="hidden"
-       animate="visible"
+      <motion.div
+        className="row"
+        variants={about_variant}
+        initial="hidden"
+        animate="visible"
       >
         {skills.map((item, index) => {
           return (
@@ -81,8 +96,7 @@ function About() {
           );
         })}
       </motion.div>
-     
-      </>
+    </>
   );
 }
 export default About;
